19ef8b870718d231a34905e9877bc7d269998971
billzhong <billzhong@gmail.com>
2013-09-21 03:39:06 -0230

    init

---
+++ README.md
@@ -0,0 +1,84 @@
+WeiYun Downloader
+=================
+
+This script will fetch WeiYun's real URL and call `wget` to download.
+
+GetIt
+-----
+
+###You need [python](http://www.python.org) to run this script.
+
+```bash
+wget https://raw.github.com/billzhong/weiyun/master/weiyun.py
+```
+
+Usage
+-----
+```
+weiyun.py [-h] [--resume] url
+```
+
+For example, the url is `http://share.weiyun.com/01118854b800d71077ffd4f37df15a2c`:
+
+```bash
+python weiyun.py http://share.weiyun.com/01118854b800d71077ffd4f37df15a2c
+```
+
+### --resume
+
+Resume getting a partially-downloaded file.
+
+For example:
+
+```bash
+python weiyun.py http://share.weiyun.com/01118854b800d71077ffd4f37df15a2c --resume
+```
+
+Note
+----
+Only tested in `python 2.7.x` and `wget 1.14`.
+
+
+
+
+中文
+====
+
+本脚本用来获取微云网盘的真实下载地址，并调用 `wget` 来下载。
+
+获取
+----
+
+需要 [Python](http://www.python.org) 环境。
+
+```bash
+wget https://raw.github.com/billzhong/weiyun/master/weiyun.py
+```
+
+用法
+----
+
+```
+weiyun.py [-h] [--resume] url
+```
+
+例如，地址是 `http://share.weiyun.com/01118854b800d71077ffd4f37df15a2c` ：
+
+```bash
+python weiyun.py http://share.weiyun.com/01118854b800d71077ffd4f37df15a2c
+```
+
+### --resume
+
+继续下载之前未下载完的文件。
+
+例如：
+
+```bash
+python weiyun.py http://share.weiyun.com/01118854b800d71077ffd4f37df15a2c --resume
+```
+
+备注
+----
+仅在 `python 2.7.x` 和 `wget 1.14` 下测试。
+
---
+++ weiyun.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import argparse
+import urllib2
+import re
+import json
+import distutils.spawn
+import subprocess
+
+
+# call wget to download
+def wget_download(download_url, file_name, cookie_name, cookie_value, resume=False):
+    wget_cmd = ['wget',
+                '--header=Cookie: ' + cookie_name + '=' + cookie_value,
+                '--referer=' + args.url, download_url, '-O', file_name]
+    if resume:
+        wget_cmd.append('-c')
+    assert distutils.spawn.find_executable(wget_cmd[0]), "Cannot find %s" % wget_cmd[0]
+    exit_code = subprocess.call(wget_cmd)
+    if exit_code != 0:
+        raise Exception('Cannot call wget to download.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='WeiYun Downloader')
+    parser.add_argument('url', help='WeiYun Share URL')
+    parser.add_argument('--resume', help='Resume getting a partially-downloaded file.', action='store_true')
+    args = parser.parse_args()
+
+    # check the url contain weiyun.com
+    if args.url.find('weiyun.com') == -1:
+        raise Exception('URL must contain weiyun.com.')
+
+    # use urllib2 to get html data
+    try:
+        request = urllib2.Request(args.url)
+        request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) \
+                            AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1')
+        html = urllib2.urlopen(request).read()
+    except:
+        raise Exception('Please check the URL.')
+
+    # check the html data contain <head> keyword
+    if html.find('<head>') == -1:
+        raise Exception('Cannot get correct html page.')
+
+    # use regexp to search the link data
+    try:
+        m = re.search('shareInfo = {(.*?)};', html)
+        data = '{' + m.group(1) + '}'
+        result = json.loads(data)
+    except:
+        raise Exception('Cannot get the link data.')
+
+    # construct url
+    url = 'http://' + result['dl_svr_host'] + ':' + str(result['dl_svr_port']) + '/ftn_handler/' \
+        + result['dl_encrypt_url'] + '/?fname=' + result['filename']
+    fn = result['filename']
+    cn = result['dl_cookie_name']
+    cv = result['dl_cookie_value']
+
+    # download file
+    if args.resume:
+        wget_download(url, fn, cn, cv, True)
+    else:
+        wget_download(url, fn, cn, cv)
